d813d8628a774f02f6a6ce97f793ba0efe865fe6
Joseph Njoku <oxeejo@gmail.com>
2024-08-26 12:27:29 +0100

    initial-commit

--- basics/variables/name.py
+++ basics/variables/name.py
@@ -1,2 +1,16 @@
+#Using case in a string with methods
+from pyexpat.errors import messages
+
 name = "mansur abdulfataih"
 print(name.title())
+print(name.upper())
+print(name.lower())
+
+#Concatenating Strings
+first_name = "joe"
+last_name = "collins"
+full_name = first_name + " " + last_name
+print(full_name)
+
+message = "Hello, " + full_name.title() + "!"
+print(message)
