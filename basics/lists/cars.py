07756b101a99cba8aa93be91aee148434d76263a
Joseph Njoku <oxeejo@gmail.com>
2024-08-26 18:23:46 +0100

    initial commit

--- basics/lists/cars.py
+++ basics/lists/cars.py
@@ -1,3 +1,13 @@
-cars = ['benz', 'rolls royce', 'toyota']
+#Sorting a list temporarily with the sorted() function
+# You can also sort in reverse alphabetical order
+cars = ['benz', 'high-lander', 'rolls royce', 'toyota']
 cars.sort(reverse=True)
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
 print(cars)
