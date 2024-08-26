3ef8503bc2b7037dd4d152f52c63c30f69394c96
Joseph Njoku <oxeejo@gmail.com>
2024-08-26 18:42:52 +0100

    initial commit

---
+++ basics/lists/working with list/magicians.py
@@ -0,0 +1,7 @@
+# Looping through an entire lis
+magicians = ['gaius', 'merlin', 'ike', 'frerein', 'alicia']
+for magician in magicians:
+    print(magician.title() + ", that was a great performance")
+    print("I can't wait to see your next trick, " + magician.title() + '.\n')
+
+print("Thank you, everyone. That was a good magic show!")
