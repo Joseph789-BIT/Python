66239b96ea502a858788a83931b05f3c2473fe65
Joseph Njoku <oxeejo@gmail.com>
2024-08-27 11:47:37 +0100

    initial commit

--- basics/lists/working with list/magicians.py
+++ basics/lists/working with list/magicians.py
@@ -2,6 +2,6 @@
 magicians = ['gaius', 'merlin', 'ike', 'frerein', 'alicia']
 for magician in magicians:
     print(magician.title() + ", that was a great performance")
-    print("I can't wait to see your next trick, " + magician.title() + '.\n')
 
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
 print("Thank you, everyone. That was a good magic show!")
