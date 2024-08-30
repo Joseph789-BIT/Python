131916aa108c770ce510329c93331eb83dd962ae
Joseph Njoku <oxeejo@gmail.com>
2024-08-30 12:01:59 +0100

    initial-commit

--- basics/functions/user_profile.py
+++ basics/functions/user_profile.py
@@ -2,4 +2,12 @@ def build_profile(first, last, **user_info):
     """Build a dictionary containing everything"""
     profile = {}
     profile['first_name'] = first
-    profile['last_name'] =
+    profile['last_name'] = last
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
+
+user_profile = build_profile('cid', 'kelvin',
+                             location='london',
+                             field='physics')
+print(user_profile)
