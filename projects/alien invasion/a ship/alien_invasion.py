a54edceee96c39b2e2bbbca030ab03853633da00
Joseph Njoku <oxeejo@gmail.com>
2024-09-07 15:25:27 +0100

    Initial-commit

--- projects/alien invasion/a ship/alien_invasion.py
+++ projects/alien invasion/a ship/alien_invasion.py
@@ -1,21 +1,23 @@
-
 import sys
+
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_function as gf
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
-    pygame.display.set_caption("Alien Conqueror")
+    pygame.display.set_caption("Alien Invasion")
 
     ship = Ship(screen)
 
     while True:
-
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
--- projects/alien invasion/a ship/ship.py
+++ projects/alien invasion/a ship/ship.py
@@ -7,7 +7,7 @@ class Ship():
         self.screen = screen
 
 
-        self.image = pygame.image.load()
+        self.image = pygame.image.load('images/rocket.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
