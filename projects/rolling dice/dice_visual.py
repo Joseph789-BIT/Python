4ade7da381c0a2a70bc68a8e629d61ed5beca3d4
Joseph Njoku <oxeejo@gmail.com>
2024-09-13 15:15:22 +0100

    initial-commit

--- projects/rolling dice/dice_visual.py
+++ projects/rolling dice/dice_visual.py
@@ -5,16 +5,16 @@ from die import Die
 die_1 = Die()
 die_2 = Die()
 
-result = []
+results = []
 for roll_num in range(1000):
     result = die_1.roll() + die_2.roll()
-    result.append(result)
+    results.append(result)
 
 frequencies = []
 max_result = die_1.num_sides + die_2.num_sides
 
 for value in range(2, max_result+1):
-    frequency = result.count(value)
+    frequency = results.count(value)
     frequencies.append(frequency)
 
 hist = pygal.Bar()
@@ -24,4 +24,5 @@ hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 hist._x_title = "Result"
 hist._y_title = "Frequency of Result"
 
-hist.add()
+hist.add('D6 + D6', frequencies)
+hist.render_to_file('dice_visual.svg')
