e4da1c710082f2906a5032e4ec8418f7005a39cc
Joseph Njoku <oxeejo@gmail.com>
2024-10-04 12:05:54 +0100

    initial-commit

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -1,15 +1,12 @@
-"""Defines URL patterns for learning_logs."""
-
-from django.urls import path
-
 from . import views
+from django.urls import path, include
 
 app_name = 'learning_logs'
 
 urlpatterns = [
-    # Home page
     path('', views.index, name='index'),
-
-    # Show all topics.
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('', include(('learning_logs.urls', 'learning_logs'), namespace='learning_logs')),
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
