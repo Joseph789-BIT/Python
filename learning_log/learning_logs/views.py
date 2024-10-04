63cd8dd9a599e7c88f93c15530154d4aba950858
Joseph Njoku <oxeejo@gmail.com>
2024-10-04 13:52:50 +0100

    initial-commit

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -43,4 +43,15 @@ def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
 
     if request.method != 'POST':
-        # No data
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                        args=[topic_id]))
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
